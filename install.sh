ca0821fb3734127e7ff669158d24d9d39a49a8ef
WALM <lbhnik12@gmail.com>
2025-05-17 17:51:49 +0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,6 +5,8 @@ sudo systemctl stop wayset
 pip install --break-system-packages keyboard | pip3 install --break-system-packages keyboard
 sudo pip install --break-system-packages keyboard | sudo pip3 install --break-system-packages keyboard
 
+chmod +x wayset
+
 sudo cp wayset /usr/local/bin/wayset
 sudo cp -r usr /
 cp wayset.desktop ~/.local/share/applications/wayset.desktop
